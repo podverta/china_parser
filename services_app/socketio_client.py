dfca48475eed3892d786683d5e76d14a4953c46d
Ivanov Dmitry <voa.ivanov@yandex.ru>
2024-08-05 17:26:30 +0300

    Изменены данные, которые приходят с парсеров.

--- services_app/socketio_client.py
+++ services_app/socketio_client.py
@@ -1,36 +1,39 @@
 import os
 import socketio
 from dotenv import load_dotenv
+from app.logging import setup_logger
+
 
 load_dotenv()
 SOCKETIO_URL = os.getenv('SOCKETIO_URL')
 SOCKET_KEY = os.getenv('SOCKET_KEY')
 
+logger = setup_logger('socketio', 'socketio.log')
 
 class SocketIOClient:
     def __init__(self):
         self.url = SOCKETIO_URL
         self.socket_key = SOCKET_KEY
-        self.sio = socketio.AsyncSimpleClient()
+        self.sio = socketio.AsyncClient()
 
     async def connect(self) -> None:
         """
-        Устанавливает соединение с сервером Socket.IO.
+        Establishes a connection to the Socket.IO server.
         """
         try:
             await self.sio.connect(self.url, auth={'socket_key': self.socket_key})
-            print(f"Connected to Socket.IO server at {self.url}")
+            logger.info(f"Connected to Socket.IO server at {self.url}")
         except Exception as e:
-            print(f"Failed to connect to Socket.IO server: {e}")
+            logger.info(f"Failed to connect to Socket.IO server: {e}")
             raise
 
     async def disconnect(self) -> None:
         """
-        Закрывает соединение с сервером Socket.IO.
+        Disconnects from the Socket.IO server.
         """
         try:
             await self.sio.disconnect()
-            print("Disconnected from Socket.IO server")
+            logger.info("Disconnected from Socket.IO server")
         except Exception as e:
-            print(f"Failed to disconnect from Socket.IO server: {e}")
+            logger.info(f"Failed to disconnect from Socket.IO server: {e}")
             raise
