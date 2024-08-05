f29468ab29de8528ea4a8597222843e7eec80fa9
Ivanov Dmitry <voa.ivanov@yandex.ru>
2024-08-05 16:31:16 +0300

    Изменены данные, которые приходят с парсеров.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 # main.py
-
 import uvicorn
 from app.app_factory import create_app
 
--- transfer_data/data_handler.py
+++ transfer_data/data_handler.py
@@ -1,11 +1,9 @@
+# redis_handler.py
 import json
+from typing import Optional
 from app.main import app
 
-
-async def send_and_save_data(
-        data: str,
-        service_name: str
-) -> None:
+async def send_and_save_data(data: str, service_name: str) -> None:
     """
     Отправляет данные на Socket.IO сервер и сохраняет их в Redis.
 
@@ -31,30 +29,43 @@ async def send_and_save_data(
     except Exception as e:
         print(f'Error sending data: {str(e)}')
 
-async def set_load_data_resid(
-        data: str,
-        service_name: str
-) -> None:
+async def set_load_data_redis(action: str, key: str, data: Optional[dict] = None) -> Optional[dict]:
     """
-    Отправляет данные на Socket.IO сервер и сохраняет их в Redis.
+    Сохраняет или загружает данные из Redis в зависимости от действия.
 
-    :param data: Данные для отправки и сохранения.
-    :param service_name: Наименование сервиса откуда пришли данные
+    :param action: Действие - 'save' для сохранения, 'load' для загрузки.
+    :param key: Ключ для данных.
+    :param data: Данные для сохранения (только для действия 'save').
+    :return: Загруженные данные (если действие 'load'), иначе None.
     """
     try:
-        # Получаем доступ к инициализированным клиентам
+        # Получаем доступ к инициализированному клиенту Redis
         redis = app.state.redis
 
-        # Преобразование данных в JSON
-        json_data = json.dumps(data, ensure_ascii=False)
+        if action == "save":
+            if data is None:
+                raise ValueError("Data must be provided for 'save' action")
 
-        # Отправка данных на сервер Socket.IO
-        await sio.emit('message', json_data)
+            # Преобразование данных в JSON
+            json_data = json.dumps(data, ensure_ascii=False)
+            await redis.set(key, json_data)
+            print(f"Data saved to Redis with key: {key}")
 
-        # Сохранение данных в Redis
-        await redis.set(service_name, json_data)
+        elif action == "load":
+            # Загрузка данных из Redis
+            json_data = await redis.get(key)
+            if json_data is None:
+                print(f"No data found for key: {key}")
+                return None
 
-        print("Data sent to Socket.IO and saved to Redis.")
+            # Преобразование данных из JSON
+            data = json.loads(json_data.decode('utf-8'))
+            print(f"Data loaded from Redis with key: {key}")
+            return data
+
+        else:
+            raise ValueError("Action must be either 'load' or 'save'")
 
     except Exception as e:
-        print(f'Error sending data: {str(e)}')
+        print(f"Error handling Redis data: {str(e)}")
+        return None
