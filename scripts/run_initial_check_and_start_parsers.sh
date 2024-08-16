15da2e4a4936f3bb73336e7392589feadae2fce5
Ivanov Dmitry <voa.ivanov@yandex.ru>
2024-08-16 13:13:22 +0300

    Добавлен новый список для единого перевода с двух сайтов, функция для проверки этого списка перед запуском парсеров. Убраны лишние пробелы при переводе.

--- scripts/run_initial_check_and_start_parsers.sh
+++ scripts/run_initial_check_and_start_parsers.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Загрузка переменных окружения из .env файла
-source /var/www/api.parserchina.com/china_parser/.env
+/var/www/api.parserchina.com/china_parser/.env
 
-# Запуск задачи check_and_start_parsers_akty с аргументом is_first_run=True
-/var/www/api.parserchina.com/china_parser/.venv/bin/celery -A services_app.celery_app call services_app.tasks.check_and_start_parsers_akty --args='[true]'
-
-# Запуск задачи check_and_start_parsers_fb с аргументом is_first_run=True
-/var/www/api.parserchina.com/china_parser/.venv/bin/celery -A services_app.celery_app call services_app.tasks.check_and_start_parsers_fb --args='[true]'
+# Запуск задачи check_and_start_parsers с аргументом is_first_run=True
+/var/www/api.parserchina.com/china_parser/.venv/bin/celery -A services_app.celery_app call services_app.tasks.check_and_start_parsers --args='[true]'
